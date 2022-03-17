89a8ab77739bd08b323ee72e083e506645c51f7e
Jake Shimota <shimo035@umn.edu>
2022-03-17 15:37:39 +0000

    Added the Create Product service

--- service/routes.py
+++ service/routes.py
@@ -1,34 +1,65 @@
 """
-My Service
+Product Service
 
-Describe what your service does here
+Paths:
+------
+GET /products - Returns a list all of the Products
+GET /products/{id} - Returns the Product with a given id number
+POST /products - creates a new Product record in the database
+PUT /products/{id} - updates a Product record in the database
+DELETE /products/{id} - deletes a Product record in the database
 """
 
 import os
 import sys
 import logging
 from flask import Flask, jsonify, request, url_for, make_response, abort
+from werkzeug.exceptions import NotFound
+from service.models import Product, DataValidationError
 from . import status  # HTTP Status Codes
+from . import app  # Import Flask application
 
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
 from flask_sqlalchemy import SQLAlchemy
-from service.models import YourResourceModel, DataValidationError
-
-# Import Flask application
-from . import app
 
 ######################################################################
 # GET INDEX
 ######################################################################
 @app.route("/")
 def index():
-    """ Root URL response """
+    """Root URL response"""
+    app.logger.info("Request for Root URL")
     return (
-        "Reminder: return some useful information in json format about the service here",
+        jsonify(
+            name="Product REST API Service",
+            version="1.0",
+            paths=url_for("list_products", _external=True),
+        ),
         status.HTTP_200_OK,
     )
 
+######################################################################
+# CREATE A NEW PRODUCT
+######################################################################
+@app.route("/products", methods=["POST"])
+def create_products():
+    """
+    Creates a Product
+    This endpoint will create a Product based the data in the body that is posted
+    """
+    app.logger.info("Request to create a product")
+    check_content_type("application/json")
+    product = Product()
+    product.deserialize(request.get_json())
+    product.create()
+    message = product.serialize()
+    location_url = url_for("get_products", product_id=product.id, _external=True)
+
+    app.logger.info("Product with ID [%s] created.", product.id)
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+    )
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
@@ -38,4 +69,15 @@ def index():
 def init_db():
     """ Initializes the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    Product.init_db(app)
+
+def check_content_type(media_type):
+    """Checks that the media type is correct"""
+    content_type = request.headers.get("Content-Type")
+    if content_type and content_type == media_type:
+        return
+    app.logger.error("Invalid Content-Type: %s", content_type)
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        "Content-Type must be {}".format(media_type),
+    )
